6a93733ebbd5b5e376704008583515d5d50f950d
Krissssskrisss <vasyliv1309@gmail.com>
2020-02-12 17:22:31 +0200

    Initial commit

---
+++ src/test/java/com/example/demo/GoogleTest.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Selenide;
+import org.openqa.selenium.By;
+
+//import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+public class GoogleTest {
+
+    @Test
+    void testOpenGoogle() {
+        Selenide.open("http://google.com");
+        Selenide.$(By.xpath("//input[@name='q']")).setValue("Hello world").pressEnter();
+
+        Selenide.$(By.partialLinkText("wikip22edia")).shouldHave(text("Hello world!"));
+    }
+
+
+
+}
